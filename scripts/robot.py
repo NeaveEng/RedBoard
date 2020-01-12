1d1d2cad7671bba73948f9781b4d0814ae9e8cd0
Tom Oinn <tomoinn@gmail.com>
2020-01-12 00:34:51 +0000

    More LEDs...

--- scripts/robot.py
+++ scripts/robot.py
@@ -1,65 +1,91 @@
 # Simple robot using approxeng.input and redboard
+# Do pip3 install approxeng.input approxeng.task and attach a controller
 from approxeng.input.selectbinder import ControllerResource
 from redboard import RedBoard, Display
 from time import sleep
+from approxeng.task import task, run, register_resource
+from approxeng.task.menu import register_menu_tasks_from_yaml, MenuTask, MenuAction
+
+# Initialise the RedBoard and its attached OLED display
+register_resource(name='board', value=RedBoard())
+display = Display()
+register_resource(name='display', value=display)
+
+
+class MenuControllerTask(MenuTask):
+    """
+    Use a connected gamepad as menu navigation, write out menus to the redboard's display module
+    """
+
+    def get_menu_action(self, world):
+        if 'dleft' in world.joystick.presses:
+            return MenuAction.previous
+        if 'dright' in world.joystick.presses:
+            return MenuAction.next
+        if 'cross' in world.joystick.presses:
+            return MenuAction.select
+        if 'dup' in world.joystick.presses:
+            return MenuAction.up
+
+    def display_menu(self, world, title, item_title, item_index, item_count):
+        world.display.text(line1=title, line2=item_title, line3='{}/{}'.format(item_index + 1, item_count))
 
 
 def mixer(yaw, throttle):
-    """
-    Convert yaw / throttle to a pair of motor speeds
-
-    :param yaw:
-        float between -1.0 to turn left, 1.0 to turn right
-    :param throttle:
-        float between -1.0 for full reverse and 1.0 for full forwards
-    :return:
-        (left, right) motor values in the -1.0 to 1.0 range
-    """
     left = throttle + yaw
     right = throttle - yaw
     scale = 1.0 / max(1, abs(left), abs(right))
     return int(left * scale), int(right * scale)
 
 
-class RobotStopException(Exception):
-    """
-    Raised when we want to break out of the loop
-    """
-    pass
+@task(name='drive')
+def manual_control(joystick, display, board):
+    if 'home' in joystick.presses:
+        return 'stop'
+    lx, ly = joystick['lx', 'ly']
+    board.motor0, board.motor1 = mixer(yaw=lx, throttle=ly)
+    display.text(line1='Simple Robot Script',
+                 line2='x={:.2f}, y={:.2f}'.format(lx, ly),
+                 line3='Press HOME to exit.')
+    if ly >= 0:
+        board.set_led(ly / 4, 1, ly)
+    else:
+        board.set_led(1 + ly / 4, 1, abs(ly))
 
 
-# Initialise the RedBoard and its attached OLED display
-board = RedBoard()
-display = Display()
-
-try:
-    while True:
-        # Loop forever until something raises RobotStopException
-        try:
-            with ControllerResource() as joystick:
-                # Bound to a joystick, update the display and start driving
-                display.text(line1='Simple Robot Script', line2='Off we go!')
-                while joystick.connected:
-                    # Loop while connected
-                    joystick.check_presses()
-                    if 'home' in joystick.presses:
-                        # Home button pressed, bail out
-                        raise RobotStopException()
-                    # Read left X and Y values from the controller, feed to mixer, insert into
-                    # motor properties
-                    lx, ly = joystick['lx', 'ly']
-                    board.motor0, board.motor1 = mixer(yaw=lx, throttle=ly)
-                    display.text(line1='Simple Robot Script',
-                                 line2='x={:.2f}, y={:.2f}'.format(lx, ly),
-                                 line3='Press HOME to exit.')
-                    if ly >= 0:
-                        board.set_led(ly / 4, 1, ly)
-                    else:
-                        board.set_led(1 + ly / 4, 1, abs(ly))
-        except IOError:
-            # Raised if there's no available controller, display this information
-            display.text(line1='Simple Robot Script', line3='Waiting for Controller')
-            sleep(1)
-except RobotStopException:
-    # Shut everything down and exit
+@task(name='stop')
+def turn_off(board):
     board.stop()
+    return 'main_menu'
+
+
+register_menu_tasks_from_yaml('robot_menus.yml')
+
+while True:
+    # Loop forever until something raises RobotStopException
+    try:
+        with ControllerResource() as joystick:
+
+            # Tell the task system about the joystick
+            register_resource('joystick', joystick)
+
+
+            def check_joystick():
+                # Check whether the joystick is connected
+                if not joystick.connected:
+                    # Returning True from here exists the task loop
+                    return True
+                # Check for presses before calling the active task
+                joystick.check_presses()
+                # If home button pressed, jump to the main_menu task
+                if 'home' in joystick.presses:
+                    return 'main_menu'
+
+
+            run(root_task='main_menu', check_tasks=[check_joystick])
+
+
+    except IOError:
+        # Raised if there's no available controller, display this information
+        display.text(line1='Simple Robot Script', line3='Waiting for Controller')
+        sleep(1)
